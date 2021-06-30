fbeec912299cb14a0ed6c95edc2dac7826c3f879
Mario Visic <mario@mariovisic.com>
2021-06-30 19:47:51 +1000

    Use the garmin 520 in the simulator as it is the lowest memory device on the supported list

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE = edge530
+DEVICE = edge_520
 SDK_HOME = /Users/mario.visic/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk
 DEPLOY = /Volumes/GARMIN/Garmin/Apps
 PRIVATE_KEY = /Users/mario.visic/.ssh/garmin.der
